7b379f3a40a846c51b75d7fbffcf7800c9f990d4
hkarabey <99493517+hkarabey@users.noreply.github.com>
2022-09-20 13:07:03 +0300

    pusla

--- src/test/java/com/popLeads/UITest/TestUI.java
+++ src/test/java/com/popLeads/UITest/TestUI.java
@@ -1,15 +1,42 @@
 package com.popLeads.UITest;
 
 import com.popLeads.pages.BasePage;
+import com.popLeads.utilities.BrowserUtilities;
 import com.popLeads.utilities.ConfigurationReader;
 import com.popLeads.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestUI extends BasePage {
-
+Actions actions=new Actions(Driver.getDriver());
+WebDriverWait wait=new WebDriverWait(Driver.getDriver(),10);
 @Test
     public void test(){
+Driver.getDriver().get(ConfigurationReader.getProperty("url"));
 
+    for (WebElement eachCourse : courses) {
+        System.out.println(eachCourse.getText());
+    }
+
+    course1.click();
+
+
+    actions.click(faceToFace).perform();
+
+
+    for (WebElement webElement : date) {
+        System.out.println(webElement.getText());
+    }
+wait.until(ExpectedConditions.elementToBeClickable(date1));
+date1.click();
+    add.click();
+
+    Assert.assertTrue(kontrol.equals("Added To Cart"));
 }
 
 
--- src/test/java/com/popLeads/pages/BasePage.java
+++ src/test/java/com/popLeads/pages/BasePage.java
@@ -5,12 +5,32 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public abstract class BasePage {
     public BasePage() {
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
-    @FindBy(xpath = "(//a[@class='Button'])[1]")
-    public WebElement xxx;
+    @FindBy(xpath = "//div[@class='sc-18ecabe2-0 sc-18ecabe2-1 hyuaVJ fCWyKC']//div[@class='sc-18ecabe2-0 sc-909ce6e0-0 djRASc kRfcpp']")
+    public List<WebElement> courses;
+
+    @FindBy(xpath = "(//div[.='Facility Management Professional® - Review Course'])[2]")
+    public WebElement course1;
+
+    @FindBy(xpath = "((//div[@class='sc-18ecabe2-0 flVutI'])[3]//div[1]")
+    public WebElement faceToFace;
+
+    @FindBy(xpath = "//label[@class='sc-18ecabe2-0 flVutI']")
+    public List<WebElement> date;
+
+    @FindBy(xpath = "(//label[@class='sc-18ecabe2-0 flVutI'])[1]")
+    public WebElement date1;
+
+    @FindBy(xpath = "//button[@aria-label='Add to cart']")
+    public WebElement add;
+
+    @FindBy(xpath = "//div[@class='sc-18ecabe2-0 cQzevD']")
+public WebElement kontrol;
 
 }
